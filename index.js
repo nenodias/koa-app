41f5a839db1138009cc6c1516f03cd712e6a49d5
Horácio Dias <horacio.dias@yahoo.com>
2018-02-08 22:20:19 -0200

    Updating projet with koa-parser

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
 const Koa = require('koa');
-const app = new Koa();
-
+const Router = require('koa-router');
+const bodyParser = require('koa-parser');
 const PORT = 3000;
 
-app.use(async (ctx, next) =>{
-	console.log(`${ctx.method} ${ctx.url} ${new Date()}`);
-	return await next();
+const app = new Koa();
+const router = new Router();
+
+app.use(bodyParser());
+
+let posts = [
+	{
+		"id":1,
+		"name":"Nodejs Developer",
+		"content":"This look nice"
+	},
+	{
+		"id":2,
+		"name":"Python Developer",
+		"content":"This is nice, good work!"
+	},
+	{
+		"id":3,
+		"name":"Cobol Developer",
+		"content":"Help!I need run of this life!"
+	}
+];
+
+
+router.get('/', (ctx, next) => {
+	ctx.body = "Hello world";
 });
 
-app.use(async (ctx, next) => {
-	console.log(`2nd middleware`);
-	return await next();
+router.get('/posts', (ctx, next) => {
+	ctx.body = posts;
 });
 
-app.use(async (ctx, next) => {
-	ctx.body = "Hello world!!!";
+router.post('/posts', (ctx, next) => {
+	let {id, name, content} = ctx.request.body;
+	if(!id){
+		ctx.throw(400, 'id is required field');
+	}
+	if(!name){
+		ctx.throw(400, 'name is required field');
+	}
+	if(!content){
+		ctx.throw(400, 'content is required field');
+	}
+	posts.push({id, name, content});
+	ctx.body = posts;
 });
+
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(PORT);
 console.log(`Server is listening on PORT ${PORT}`);
