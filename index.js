227f038a9cddf7bb357e7f378f65732175eb2ccc
Horácio Dias <horacio.dias@yahoo.com>
2018-02-14 19:31:51 -0200

    Finalizing lesson 25th

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-parser');
 const PORT = 3000;
+const _ = require('lodash');
 
 const app = new Koa();
 const router = new Router();
@@ -50,6 +51,45 @@ router.post('/posts', async(ctx, next) => {
 	ctx.body = posts;
 });
 
+
+router.get('/posts/:id', async(ctx) => {
+	let id = parseInt(ctx.params.id);
+	let post = posts.find( post => post.id === id);
+	if(post === undefined){
+		ctx.throw(404, 'post not found');
+	}
+	ctx.body = post;
+});
+
+router.delete('/posts/:id', async(ctx) => {
+	let id = parseInt(ctx.params.id);
+	let post = _.remove(posts, post => post.id === id);
+	if(post === undefined){
+		ctx.throw(404, 'post not found');
+	}else{
+		ctx.body = post;
+	}
+});
+
+router.put('/posts/:id', async(ctx) => {
+	let findId = parseInt(ctx.params.id);
+	let { id, name, content } = ctx.request.body;
+	let index = _.findIndex(posts, post => post.id === findId);
+	if(index === -1){
+		ctx.throw(404, 'post not found');
+	}else{
+		if(id){
+			posts[index].id = id;
+		}
+		if(name){
+			posts[index].name = name;
+		}
+		if(content){
+			posts[index].content = content;
+		}
+		ctx.body = posts[index];
+	}
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
