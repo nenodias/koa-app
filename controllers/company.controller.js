5dc4c4fb9974b12f2d6db0532597c114c45b055e
Horácio Dias <horacio.baptista@supportweb.com>
2018-02-19 10:49:46 -0300

    Finally ends the CRUD

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -19,7 +19,41 @@ module.exports = {
     },
     async findOne(ctx){
         try{
-            ctx.body = await ctx.db.Company.findOne({where: {id: ctx.params.id} });
+            const company = await ctx.db.Company.findOne({where: {id: ctx.params.id} });
+            if(!company){
+                ctx.throw(404, 'company id is invalid');
+            }
+            ctx.body = company;
+        }catch(err){
+            ctx.throw(500, err);
+        }
+    },
+    async destroy(ctx){
+        try{
+            const results = await ctx.db.Company.destroy({where: {id: ctx.params.id} });
+            results === 0 ? ctx.throw(500, 'invalid id provided') : ctx.body = `company is deleted with id ${ctx.params.id}`;
+        }catch(err){
+            ctx.throw(500, err);
+        }
+    },
+    async update(ctx){
+        try{
+            let data = {};
+            if(ctx.request.body.name){
+                data["name"] = ctx.request.body.name;
+            }
+            if(ctx.request.body.city){
+                data["city"] = ctx.request.body.city;
+            }
+            if(ctx.request.body.address){
+                data["address"] = ctx.request.body.address;
+            }
+            const results = await ctx.db.Company.update(data,
+                {
+                    where: {id: ctx.params.id} 
+                }
+            );
+            results === 0 ? ctx.throw(500, 'invalid id provided') : ctx.body = `company is update with id ${ctx.params.id}`;
         }catch(err){
             ctx.throw(500, err);
         }
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ const {CompanyController} = require('../controllers');
 router.post('/companies', CompanyController.create);
 router.get('/companies', CompanyController.find);
 router.get('/companies/:id', CompanyController.findOne);
+router.delete('/companies/:id', CompanyController.destroy);
+router.put('/companies/:id', CompanyController.update);
 
 module.exports = router;
