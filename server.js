23b243d4afab5f867f99e7c583d329572750aac4
Horácio Dias <horacio.dias92@gmail.com>
2018-02-23 22:09:58 -0300

    Adding a static middleware for serving html

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-parser');
 const _ = require('lodash');
+const serve = require('koa-static');
 const PORT = 4000;
 
 const router = require('./routes');
@@ -14,12 +15,9 @@ db.sequelize.authenticate()
     .then(() => console.log('Models sychronized!'))
     .catch((err) => console.error(err));
 
-router.get('/', async (ctx, next) => {
-    ctx.body = "Hello";
-});
-
 app.context.db = db;
 app.use(bodyParser());
+app.use(serve(__dirname + "/public"))
 app.use(router.routes());
 app.listen(PORT);
 console.log(`Server listening on port ${PORT}`);
